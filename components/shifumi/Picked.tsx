a67f3405a2a07a412192180888181a5704de1d2c
Johan Soulet <johan.soulet@openclassrooms.com>
2022-12-08 17:02:11 +0100

    :bug: picked component is not supposed to have a BG

--- components/shifumi/Picked.tsx
+++ components/shifumi/Picked.tsx
@@ -12,18 +12,13 @@ const StyledImage = styled(Image)`
   object-fit: contain;
 `
 
-const Background = styled.div<Pick<Props, ('player' | 'isWinner')>>`
-  background-image: url('/images/shifumi/${(props) => props.player}.svg');
-  background-position: ${(props) => props.player === 'user' ? 'left': 'right'}  top;
-  background-repeat: no-repeat;
-  background-size: auto 100%;
+const Root = styled.div<Pick<Props, ('player' | 'isWinner')>>`
   ${StyledImage} {
     mix-blend-mode: multiply;
   }
   ${props => {
     if(!props.isWinner) {
       return css`
-        background-image: none;
         ${H1} {
           visibility: hidden;
         }
@@ -43,10 +38,10 @@ const Background = styled.div<Pick<Props, ('player' | 'isWinner')>>`
 
 const Picked = ({player, choice, isWinner}: Props) => {
   const catchPhrase = player === 'user' ? 'you win' : 'computer wins'
-  return <Background player={player} isWinner={isWinner}>
+  return <Root player={player} isWinner={isWinner}>
     <H1>{catchPhrase}</H1>
     <StyledImage priority src={`/images/shifumi/${choice}.png`} height={550} width={550} alt={choice}/>
-  </Background>
+  </Root>
 }
 
 export default Picked
